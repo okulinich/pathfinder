0b22f8647c88462da9880af4f40703e77f088d56
okulinich <55047750+okulinich@users.noreply.github.com>
2019-12-31 15:14:03 +0200

    Fixed bug with an empty line at the end of file.

--- src/mx_check_all.c
+++ src/mx_check_all.c
@@ -4,14 +4,14 @@ int mx_check_all(t_lines *line, t_graph *graph, int i, char ***names_ptr) {
     int tmp[2] = {0, 1};
 
     tmp[1] = mx_read_line(&line->start, 2, '-', line->fd);
-    if (!mx_check_alpha(line->start, tmp[1], i))
-        return -3;
-    if (!mx_check_the_name(graph, line->start, names_ptr))
+    if(tmp[1] == -1 && mx_strcmp(line->start, "")  == 0)
+        return tmp[1];
+    if (!mx_check_alpha(line->start, tmp[1], i) || 
+        !mx_check_the_name(graph, line->start, names_ptr))
         return -3;
     tmp[1] = mx_read_line(&line->end, 2, ',', line->fd);
-    if (!mx_check_alpha(line->end, tmp[1], i))
-        return -3;
-    if (!mx_check_the_name(graph, line->end, names_ptr))
+    if (!mx_check_alpha(line->end, tmp[1], i) || 
+        !mx_check_the_name(graph, line->end, names_ptr))
         return -3;
     tmp[1] = mx_read_line(&line->d, 2, '\n', line->fd);
     if (!mx_check_distance(line->d) || mx_atoi(line->d) < 1 || tmp[1] < -1) {
